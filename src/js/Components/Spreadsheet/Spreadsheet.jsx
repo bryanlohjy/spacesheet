e3d97bce8416a8a77d326fdaa4af8481cd6ec82f
Bryan Loh <bryanlohjy@hotmail.com>
2018-04-09 16:11:26 +1200

    Added CellTypes file

--- src/js/Components/Spreadsheet/Spreadsheet.jsx
+++ src/js/Components/Spreadsheet/Spreadsheet.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import HotTable from 'react-handsontable';
+import Cell from './CellTypes.js';
 
 export default class Spreadsheet extends React.Component {
   constructor(props) {
