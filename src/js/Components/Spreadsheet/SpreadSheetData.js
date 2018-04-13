9ac05770116bbb4f6af1ccb40fdacf1d1f36c579
Bryan Loh <bryanlohjy@hotmail.com>
2018-04-13 23:06:44 +1200

    Added interpolation demo

--- src/js/Components/Spreadsheet/SpreadSheetData.js
+++ src/js/Components/Spreadsheet/SpreadSheetData.js
@@ -14,6 +14,8 @@ const DemoSheet = (rows, cols) => {
       [ '', "Nice. Let's build a serif operator. We can do this by finding two similar fonts - one with serifs and one without.", '', '','', "=DATAPICKER('1-6-9-0-0')", "=DATAPICKER('1-8-9-0-0')" ],
       [ '', "Let's isolate the serif operator by subtraction. We can do so by using the \"MINUS\" operator.", '', '', '' , '=MINUS(F7, G7)', '' ],
       [ '', 'We can now apply the difference to our averaged font by using the \"SUM\" operator.',  '', '' , '', '=SUM(F6, F8)' ],
+      [ '', 'Here we interpolate linearly between the two fonts using the \"LERP\" operator. We can also perform spherically by using \"SLERP\" instead.', ''],
+      [ '', '=F4', '=LERP(B11, G11, 0.2)', '=LERP(B11, G11, 0.4)', '=LERP(B11, G11, 0.6)', '=LERP(B11, G11, 0.8)', '=G4', '' ],
     ],
     mergeCells: [
       { row: 1, col: 1, rowspan: 1, colspan: 6 },
@@ -23,6 +25,7 @@ const DemoSheet = (rows, cols) => {
       { row: 6, col: 1, rowspan: 1, colspan: 4 },
       { row: 7, col: 1, rowspan: 1, colspan: 4 },
       { row: 8, col: 1, rowspan: 1, colspan: 4 },
+      { row: 9, col: 1, rowspan: 1, colspan: 6 },
     ],
   }
   if (data.data.length < (rows || 17)) {
