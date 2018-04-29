0890ed0b64238cbbcac64fcb8cd9947175505a8a
Bryan Loh <bryanlohjy@hotmail.com>
2018-04-29 18:35:36 +1200

    stricter check for slider cells

--- src/js/lib/Regex.js
+++ src/js/lib/Regex.js
@@ -1,6 +1,7 @@
 module.exports = {
   CELLS: {
     RANDFONT: new RegExp(/=\s*randfont\(.*\)$/ig),
+    SLIDER: new RegExp(/=\s*slider\(.*\)$/ig),
   },
   RANDFONT: {
     isValid: new RegExp(/RANDFONT\(\s*[\d]+\s*\)/ig),
