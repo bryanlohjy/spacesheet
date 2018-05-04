9c36f8faf249aa7649d902cc175c58d017e9fede
Bryan Loh <bryanlohjy@hotmail.com>
2018-05-04 14:46:29 +1200

    Wrap CellHelpers in object

--- src/js/Components/Spreadsheet/CellHelpers.js
+++ src/js/Components/Spreadsheet/CellHelpers.js
@@ -1,39 +1,37 @@
 import Regex from '../../lib/Regex.js';
 
-const getCellType = cellData => {
-  if (!cellData) { return; }
-  if (cellData.trim()[0] === '=') {
-    if (new RegExp(Regex.CELLS.SLIDER).test(cellData)) {
-      return 'SLIDER';
+module.exports = {
+  getCellType: cellData => {
+    if (!cellData) { return; }
+    if (cellData.trim()[0] === '=') {
+      if (new RegExp(Regex.CELLS.SLIDER).test(cellData)) {
+        return 'SLIDER';
+      }
+      if (new RegExp(Regex.CELLS.RANDFONT).test(cellData)) {
+        return 'RANDFONT';
+      }
+      return 'FORMULA';
+    } else {
+      return 'TEXT';
     }
-    if (new RegExp(Regex.CELLS.RANDFONT).test(cellData)) {
-      return 'RANDFONT';
+  },
+  cellLabelToCoords: label => {
+    const letter = label.match(/[a-z]{1}/gi);
+    const number = label.match(/([0-9]+)/gi);
+    let res;
+    if (letter && number) {
+      const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      res = {
+        row: Number(number[0]) - 1,
+        col: alphabet.indexOf(letter[0].toUpperCase()),
+      };
     }
-    return 'FORMULA';
-  } else {
-    return 'TEXT';
-  }
-}
-
-const cellLabelToCoords = label => {
-  const letter = label.match(/[a-z]{1}/gi);
-  const number = label.match(/([0-9]+)/gi);
-  let res;
-  if (letter && number) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    res = {
-      row: Number(number[0]) - 1,
-      col: alphabet.indexOf(letter[0].toUpperCase()),
-    };
-  }
-  return res;
-}
-
-const isFormula = data => {
-  if (data) {
-    return data.trim()[0] === '=';
-  }
-  return false;
-}
-
-module.exports = { getCellType, cellLabelToCoords, isFormula };
+    return res;
+  },
+  isFormula: data => {
+    if (data) {
+      return data.trim()[0] === '=';
+    }
+    return false;
+  },
+};
