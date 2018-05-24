98bc5e765e292a60439c1de8f164e26cb4a42e05
Bryan Loh <bryanlohjy@hotmail.com>
2018-05-24 13:05:04 +1200

    Wrap HotTable in its own container

--- src/js/Components/Spreadsheet/Spreadsheet.jsx
+++ src/js/Components/Spreadsheet/Spreadsheet.jsx
@@ -12,22 +12,99 @@ import { FormulaParser } from './FormulaParser.js';
 export default class Spreadsheet extends React.Component {
   constructor(props) {
     super(props);
-    this.initHotTable = this.initHotTable.bind(this);
-
-    this.state = {
+    this.state = { // updated using refs to prevent unnecessary table rendering
       inputBarIsMounted: false,
-      hotTableIsLoaded: false,
+      // inputBarValue: "",
+      currentSelection: [0, 0, 0, 0],
     };
+    this.setSelectedCellData = this.setSelectedCellData.bind(this);
+  };
+  setSelectedCellData(operation, closeAfterSetting) {
+    if (closeAfterSetting) {
+      const selection = this.hotInstance.getSelected();
+      this.hotInstance.setDataAtCell(selection[0], selection[1], operation);
+      this.inputBar.value = operation;
+      return;
+    }
+    const editor = this.hotInstance.getActiveEditor();
+    editor.beginEditing();
+    editor.clearHighlightedReferences();
+    editor.TEXTAREA.value = operation;
+    editor.eventManager.fireEvent(editor.TEXTAREA, 'keydown');
+    editor.updateTableCellCaptureClass();
+  };
+  // setInputBarValue(value) {
+  //   this.setState({ inputBarValue: value });
+  // };
+  render() {
+    return (
+      <div className="spreadsheet-container">
+        <input className="input-bar" type="text"
+          disabled
+          ref={ el => {
+            if (!this.state.inputBarIsMounted) {
+              this.inputBar = el;
+              this.setState({ inputBarIsMounted : true });
+            }
+          }}
+        />
+        <OperationDrawer
+          setSelectedCellData={this.setSelectedCellData}
+          currentSelection={this.state.currentSelection}
+        />
+        {
+          this.state.inputBarIsMounted ? (
+            <div className="table-container" ref="tableContainer">
+              <HotTableContainer
+                outputWidth={this.props.outputWidth}
+                outputHeight={this.props.outputHeight}
+                setTableRef={ ref => {
+                  this.hotInstance = ref.hotInstance;
+                  this.props.setTableRef(ref);
+                }}
+                width={this.props.width}
+                height={this.props.height}
+                inputBar={this.inputBar}
+                getCellFromDataPicker={this.props.getCellFromDataPicker}
+                model={this.props.model}
+                drawFn={this.props.drawFn}
+                decodeFn={this.props.decodeFn}
+                afterSelection={ (r, c, r2, c2) => {
+                  this.setState({
+                    currentSelection: [r, c , r2, c2],
+                  });
+                }}
+              />
+            </div>) : ''
+        }
+      </div>
+    )
+  }
+}
+Spreadsheet.propTypes = {
+  outputWidth: PropTypes.number,
+  outputHeight: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number,
+  drawFn: PropTypes.func,
+  decodeFn: PropTypes.func,
 
+  setTableRef: PropTypes.func,
+  dataPickerCellData: PropTypes.object,
+  getCellFromDataPicker: PropTypes.func,
+  model: PropTypes.object,
+};
+
+class HotTableContainer extends React.Component {
+  constructor(props) {
+    super(props);
     this.maxCols = Math.ceil(this.props.width / this.props.outputWidth);
     this.maxRows = Math.ceil(this.props.height / this.props.outputHeight);
     this.demoSheet = DemoSheet(this.maxRows, this.maxCols);
-
-    this.setSelectedCellData = this.setSelectedCellData.bind(this);
+    this.initHotTable = this.initHotTable.bind(this);
   };
   initHotTable() {
     const hotInstance = this.hotInstance;
-
     const cellTypes = new CellTypes({
       drawFn: this.props.drawFn,
       decodeFn: this.props.decodeFn,
@@ -37,7 +114,7 @@ export default class Spreadsheet extends React.Component {
         getCellFromDataPicker: this.props.getCellFromDataPicker,
         model: this.props.model,
       }),
-      inputBar: this.inputBar,
+      inputBar: this.props.inputBar,
     });
 
     hotInstance.updateSettings({
@@ -46,20 +123,20 @@ export default class Spreadsheet extends React.Component {
         const cellData = hotInstance.getDataAtRowProp(row, prop);
         switch (getCellType(cellData)) {
           case 'FORMULA':
-            cellProperties.renderer = cellTypes.Formula.renderer;
-            cellProperties.editor = cellTypes.Formula.editor;
-            break;
+          cellProperties.renderer = cellTypes.Formula.renderer;
+          cellProperties.editor = cellTypes.Formula.editor;
+          break;
           case 'SLIDER':
-            cellProperties.renderer = cellTypes.Slider.renderer;
-            cellProperties.editor = cellTypes.Slider.editor;
-            break;
+          cellProperties.renderer = cellTypes.Slider.renderer;
+          cellProperties.editor = cellTypes.Slider.editor;
+          break;
           case 'RANDFONT':
-            cellProperties.renderer = cellTypes.RandFont.renderer;
-            cellProperties.editor = cellTypes.RandFont.editor;
-            break;
+          cellProperties.renderer = cellTypes.RandFont.renderer;
+          cellProperties.editor = cellTypes.RandFont.editor;
+          break;
           default:
-            cellProperties.renderer = cellTypes.Text.renderer;
-            cellProperties.editor = cellTypes.Text.editor;
+          cellProperties.renderer = cellTypes.Text.renderer;
+          cellProperties.editor = cellTypes.Text.editor;
         }
         return cellProperties;
       },
@@ -85,114 +162,70 @@ export default class Spreadsheet extends React.Component {
     });
     hotInstance.selectCell(0, 0);
   };
-  setSelectedCellData(operation, closeAfterSetting) {
-    if (closeAfterSetting) {
-      const selection = this.hotInstance.getSelected();
-      this.hotInstance.setDataAtCell(selection[0], selection[1], operation);
-      this.inputBar.value = operation;
-      return;
-    }
-    const editor = this.hotInstance.getActiveEditor();
-    editor.beginEditing();
-    editor.clearHighlightedReferences();
-    editor.TEXTAREA.value = operation;
-    editor.eventManager.fireEvent(editor.TEXTAREA, 'keydown');
-    editor.updateTableCellCaptureClass();
+  shouldComponentUpdate(newProps, newState) {
+    return false;
   };
   render() {
-    console.warn('SPREADSHEET COMPOONENT RENDER')
     return (
-      <div className="spreadsheet-container">
-        <input className="input-bar" type="text"
-          disabled
-          ref={ el => {
-            if (!this.state.inputBarIsMounted) {
-              this.inputBar = el;
-              this.setState({ inputBarIsMounted : true });
-            }
-          }}
-        />
-        <OperationDrawer
-          setSelectedCellData={this.setSelectedCellData}
-        />
-        {
-          this.state.inputBarIsMounted ? (
-            <div className="table-container" ref="tableContainer">
-              <HotTable
-                className="table"
-                ref={ ref => {
-                  if (ref && !this.hotInstance) {
-                    this.props.setTableRef(ref);
-                    this.hotInstance = ref.hotInstance;
-                    this.initHotTable();
-                    this.setState({ hotTableIsLoaded: true });
-                  }
-                }}
-                root='hot'
+      <HotTable
+        className="table"
+        ref={ ref => {
+          if (ref && !this.hotInstance) {
+            this.props.setTableRef(ref);
+            this.hotInstance = ref.hotInstance;
+            this.initHotTable();
+          }
+        }}
+        root='hot'
 
-                mergeCells={ this.demoSheet.mergeCells }
+        mergeCells={ this.demoSheet.mergeCells }
 
-                rowHeaderWidth={32}
-                colHeaderHeight={32}
+        rowHeaderWidth={32}
+        colHeaderHeight={32}
 
-                colHeaders={true}
-                rowHeaders={true}
-                preventOverflow="horizontal"
-                rowHeights={this.props.outputHeight}
-                colWidths={this.props.outputWidth}
+        colHeaders={true}
+        rowHeaders={true}
+        preventOverflow="horizontal"
+        rowHeights={this.props.outputHeight}
+        colWidths={this.props.outputWidth}
 
-                // width={ this.props.width }
-                // height={ this.props.height - inputBarHeight - this.drawerHeight }
+        maxCols={ this.maxCols }
+        maxRows={ this.maxRows }
 
-                maxCols={ this.maxCols }
-                maxRows={ this.maxRows }
+        afterRender={ e => { console.log('HotTable Render')}}
 
-                viewportColumnRenderingOffset={26}
-                viewportRowRenderingOffset={26}
+        viewportColumnRenderingOffset={26}
+        viewportRowRenderingOffset={26}
 
-                outsideClickDeselects={false}
-                // make sure input bar is in sync
-                afterUndo={ changes => {
-                  const selection = this.hotInstance.getSelected();
-                  const data = this.hotInstance.getDataAtCell(selection[0], selection[1]);
-                  if (this.inputBar.value !== data) {
-                    this.inputBar.value = data;
-                  }
-                }}
-                afterRedo={ changes => {
-                  const selection = this.hotInstance.getSelected();
-                  const data = this.hotInstance.getDataAtCell(selection[0], selection[1]);
-                  if (this.inputBar.value !== data) {
-                    this.inputBar.value = data;
-                  }
-                }}
-                comments={true}
-                customBorders={true}
+        outsideClickDeselects={false}
+        // make sure input bar is in sync
+        afterUndo={ changes => {
+          const selection = this.hotInstance.getSelected();
+          const data = this.hotInstance.getDataAtCell(selection[0], selection[1]);
+          if (this.props.inputBar.value !== data) {
+            this.props.inputBar.value = data;
+          }
+        }}
+        afterRedo={ changes => {
+          const selection = this.hotInstance.getSelected();
+          const data = this.hotInstance.getDataAtCell(selection[0], selection[1]);
+          if (this.props.inputBar.value !== data) {
+            this.props.inputBar.value = data;
+          }
+        }}
+        afterSelection={ (r, c, r2, c2) => {
+          this.props.afterSelection(r, c, r2, c2);
+        }}
 
-                undo
-                redo
-              />
-            </div>) : ''
-        }
-      </div>
-    )
+        comments={true}
+        customBorders={true}
+
+        undo
+        redo
+      />
+    );
   }
 }
-Spreadsheet.propTypes = {
-  outputWidth: PropTypes.number,
-  outputHeight: PropTypes.number,
-  width: PropTypes.number,
-  height: PropTypes.number,
-  drawFn: PropTypes.func,
-  decodeFn: PropTypes.func,
-
-  setTableRef: PropTypes.func,
-  dataPickerCellData: PropTypes.object,
-  getCellFromDataPicker: PropTypes.func,
-  model: PropTypes.object,
-  // beforeChange: PropTypes.func,
-  // setCurrentColor: PropTypes.func,
-};
 
 class OperationDrawer extends React.Component {
   constructor(props) {
@@ -229,5 +262,6 @@ class OperationDrawer extends React.Component {
 };
 OperationDrawer.propTypes = {
   // hotInstance: PropTypes.object,
+  currentSelection: PropTypes.array,
   setSelectedCellData: PropTypes.func,
 };
