cf9d9083477bd592511ad4bcd5f21294e822de2f
Bryan Loh <bryanlohjy@hotmail.com>
2019-04-16 15:46:13 +1200

    Set min on range slider logically instead of visually

--- src/js/Components/Modal/DataPickerCreator/SelectedClass.js
+++ src/js/Components/Modal/DataPickerCreator/SelectedClass.js
@@ -10,13 +10,13 @@ export default class SelectedClass extends React.Component {
           <span>{this.props.amount.toFixed(1)*100}%</span>
           <input
             type="range"
-            min={0.1}
+            min={0}
             max={1}
             value={this.props.amount}
             step={0.1}
             onChange={(e) => {
               const id = this.props.id;
-              this.props.onClassAmountChange(id, e.target.value);
+              this.props.onClassAmountChange(id, Math.max(e.target.value, 0.1));
             }}
           />
         </div>
