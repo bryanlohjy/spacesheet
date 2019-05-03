13b0fc616e6abb3ef3fc6783d55a3af2423beba9
Bryan Loh <bryanlohjy@hotmail.com>
2019-05-03 12:23:58 +1200

    Add BigGAN class tree utils

---
+++ data_utils/biggan_csv_to_tree/index.js
@@ -0,0 +1,72 @@
+const fs = require('fs');
+
+const parseTsv = (data) => {
+  let rows = data.split('\n');
+  rows = rows.map(row => {
+    return row.split('\t');
+  });
+
+  /* Clean up */
+  rows = rows.map(row => {
+    row = row.filter(val => val && val !== '\r');
+    row = row.map(val => val.replace(/\r/gi, ''));
+    return row;
+  });
+
+  return rows;
+};
+
+const toTrailId = (trail, name) => {
+  let id = `${trail.join('-')}`.toLowerCase();
+
+  if (name) {
+    id += `-${name.toLowerCase()}`;
+  }
+
+  return id;
+}
+
+fs.readFile('./classes.tsv', 'utf8', (err, tsv) => {
+  const data = parseTsv(tsv);
+
+  let tree = [];
+
+  data.forEach((row, rowIndex) => {
+    let [name, aliases, ...classes] = row;
+
+    let pointer = tree;
+    // Create parent classes
+    classes.forEach((_class, _classIndex) => {
+      let trailId = classes.slice(0, _classIndex+1);
+      trailId = trailId.join('-').toLowerCase();
+
+      let parent = pointer.find(child => child.trailId === trailId);
+
+      if (!parent) {
+        parent = {
+          id: Math.random(),
+          trailId: trailId,
+          name: _class,
+          children: []
+        };
+        pointer.push(parent);
+      }
+      pointer = parent.children;
+
+      const endOfTrail = _classIndex === classes.length-1;
+
+      if (endOfTrail) {
+        const newClass = {
+          id: Math.random(),
+          trailId: `${trailId}-${name}`,
+          name,
+          children: []
+        };
+
+        pointer.push(newClass);
+      }
+    });
+  });
+
+  fs.writeFile('tree.json', JSON.stringify(tree, null, 2), 'utf8', () => {});
+});
