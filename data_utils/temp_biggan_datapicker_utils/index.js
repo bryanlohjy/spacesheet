877deee7bd9b0e2e9d1112b1ff3eb4600c17fcba
Bryan Loh <bryanlohjy@hotmail.com>
2019-05-07 14:39:11 +1200

    Script to create variety BigGAN datapicker

--- data_utils/temp_biggan_datapicker_utils/index.js
+++ data_utils/temp_biggan_datapicker_utils/index.js
@@ -9,6 +9,7 @@ const tf = require('@tensorflow/tfjs');
 const base64ImageToFile = require('base64image-to-file');
 
 const latentDims = 140;
+const classDims = 1000;
 
 /* Create list of latent vectors */
 // const randVector = () => {
@@ -30,30 +31,52 @@ const latentDims = 140;
 // let vectors = createVectors(25);
 // fs.writeFile('latentVectors.json', JSON.stringify(vectors, null, 2), 'utf8', () => {});
 // return;
-/* Decode latents into images */
-// const modelUrl = "http://deeptom.staff.vuw.ac.nz:5000";
-// const decodeVector = (vec, label) => {
-//   const decodeUrl = `${modelUrl}/decode`;
-//   return fetch(decodeUrl, {
-//     method: 'POST',
-//     headers: {
-//       'Accept': 'application/json',
-//       'Content-Type': 'application/x-www-form-urlencoded'
-//     },
-//     body: `vector=${JSON.stringify(vec)}&label=${JSON.stringify(label)}`
-//   });
-// }
+
+/* Create distribution of labels from BigGAN Classes */
+// const createLabelsFile = (numLabels) => {
+//   let labels = [];
 //
-// const latents = require('./latentVectors.json');
+//   const classInterval = Math.floor(classDims/numLabels);
 //
-const strawBananaLabel =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.4,0,0,0,0.1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-// const label = strawBananaLabel;
+//   for (let labelIndex=0; labelIndex<numLabels; labelIndex++) {
+//     let label = new Array(classDims).fill(0);
+//     const classIndex = labelIndex*classInterval;
 //
-// async function asyncForEach(array, callback) {
-//   for (let index = 0; index < array.length; index++) {
-//     await callback(array[index], index, array);
+//     label[classIndex] = 1;
+//     labels.push(label);
 //   }
+//
+//   fs.writeFile('labels.json', JSON.stringify(labels), 'utf8', () => {});
 // }
+//
+// createLabelsFile(1000);
+//
+// return;
+
+/* Decode latents into images */
+const modelUrl = "http://deeptom.staff.vuw.ac.nz:5000";
+const decodeVector = (vec, label) => {
+  const decodeUrl = `${modelUrl}/decode`;
+  return fetch(decodeUrl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `vector=${JSON.stringify(vec)}&label=${JSON.stringify(label)}`
+  });
+}
+// DataPicker for Single Class
+// const latents = require('./latentVectors.json');
+// // //
+// const strawBananaLabel =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+// const label = strawBananaLabel;
+// //
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
 //
 // const start = async () => {
 //   let decodedLatents = [];
@@ -74,8 +97,9 @@ const strawBananaLabel =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 //   fs.writeFile('decodedLatents.json', JSON.stringify(decodedLatents, null, 2), 'utf8', () => {});
 // }
 // start();
-
-/* Latents to DataPicker */
+// // return
+// //
+// // /* Latents to DataPicker */
 const decodedLatents = require('./decodedLatents.json');
 const dataPickerSize = 5;
 
@@ -102,7 +126,7 @@ decodedLatents.forEach((latent, index) => {
     base64: latent.image,
     row: row,
     column: col,
-    vector: latent.latent.concat(strawBananaLabel)
+    vector: latent.latent.concat(latent.label)
   }
 
   const pickerKey = `${col}-${row}`;
@@ -110,3 +134,53 @@ decodedLatents.forEach((latent, index) => {
 });
 
 fs.writeFile('dataPickerGrid.json', JSON.stringify(dataPickerGrid), 'utf8', () => {});
+
+/* Cherrypick utils */
+// let cherrypickedLabels = [];
+// fs.readdir('./reconstructions/biggan-all/cherrypicked', (err, names) => {
+//   names.forEach(name => {
+//     const bigGANClassIndex = name.replace('image-', '').replace('.jpeg', '');
+//     cherrypickedLabels.push(Number(bigGANClassIndex))
+//   });
+//   fs.writeFile('cherrypickedLabels.json', JSON.stringify(cherrypickedLabels, null), 'utf8', () => {});
+// });
+
+/* Multi-labelled Datapicker (latent is constant)*/
+// const labels = require('./labels.json');
+
+/* Reduce cherrypicked to num*/
+// const cherrypickedIndexes = require('./cherrypickedLabels');
+// const numToKeep = 25;
+//
+// let shuffled = cherrypickedIndexes.sort(() => 0.5 - Math.random());
+// let reducedCherrypicked = shuffled.slice(0, 25);
+// reducedCherrypicked = reducedCherrypicked.map(index => {
+//   let oneHot = new Array(classDims).fill(0);
+//   oneHot[index] = 1;
+//   return oneHot;
+// });
+// fs.writeFile('reducedCherrypicked.json', JSON.stringify(reducedCherrypicked, null), 'utf8', () => {});
+
+// const cherrypicked = require('./reducedCherrypicked.json');
+//
+// const start = async () => {
+//   let decodedLatents = [];
+//   const latent = new Array(latentDims).fill(0);
+//
+//   await asyncForEach(cherrypicked, async (label, index) => {
+//     let image = await decodeVector(latent, label);
+//     image = await image.json();
+//     base64ImageToFile(image, './reconstructions/', `image-${index}`, () => {});
+//
+//     const decodedLatent = {
+//       latent,
+//       image,
+//       label
+//     };
+//     console.log(index)
+//     decodedLatents.push(decodedLatent);
+//   });
+//
+//   fs.writeFile('decodedLatents.json', JSON.stringify(decodedLatents, null, 2), 'utf8', () => {});
+// }
+// start();
